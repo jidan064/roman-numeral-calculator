a1d704e2794f279c6df07785238c94e3d27ef0da
Dan <jidan064@gmail.com>
2016-06-17 20:51:08 -0700

    Update test files.

---
+++ RomanNumeral.h
@@ -0,0 +1,20 @@
+/*********************************************************
+ * Description: A simple Roman Numeral Calculator with ADD/SUB functions
+ * Date:        6/17/2016
+ * Author:      Ji, Dan
+*********************************************************/
+
+#ifndef _ROMAN_NUMERAL_CALCULATOR_
+#define _ROMAN_NUMERAL_CALCULATOR_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+// Add function
+// output = str1 + str2
+char* add(char* str1, char* str2);
+
+// Subtract function
+// output = str1 - str2
+char* sub(char* str1, char* str2);
+
+#endif
